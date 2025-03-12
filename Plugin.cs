cb91f779fd413648ecd2579a0180176ed23292b7
Thomas Gruber <tgr@lookat.at>
2025-03-12 23:13:15 +0100

    added feature to configure the Freeplay onDeath respawn time

--- Plugin.cs
+++ Plugin.cs
@@ -1,16 +1,47 @@
 ﻿using BepInEx;
+using BepInEx.Configuration;
+using GameEvent;
 using HarmonyLib;
+using UnityEngine;
 
 namespace UCHQoLPatches;
 
 [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
-public class Plugin : BaseUnityPlugin
+public class Plugin : BaseUnityPlugin, IGameEventListener
 {
+    private ConfigEntry<float> _maxDeathDelay;
+
     private void Awake()
     {
         new Harmony("uch.patch.qol.xxxprod.com").PatchAll();
 
-        // Plugin startup logic
+        _maxDeathDelay = Config.Bind("FreePlay", "On Death Respawn Delay", 0.3f, "The delay in seconds until the character is respawned after death.");
+        _maxDeathDelay.SettingChanged += MaxDeathDelay_SettingChanged;
+
+        GameEventManager.ChangeListener<StartPhaseEvent>(this, true);
+
         Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+    }
+
+    private void MaxDeathDelay_SettingChanged(object sender, System.EventArgs e)
+    {
+        UpdateMaxDeathDelay();
+    }
+
+    public void handleEvent(GameEvent.GameEvent e)
+    {
+        if (e is StartPhaseEvent)
+            UpdateMaxDeathDelay();
+    }
+
+    private void UpdateMaxDeathDelay()
+    {
+        Character[] characters = FindObjectsOfType<Character>();
+        foreach (Character character in characters)
+        {
+            character.maxDeathDelay = _maxDeathDelay.Value;
+        }
+
+        Debug.Log("Updated MaxDeathDelay on all characters.");
     }
 }
--- README.md
+++ README.md
@@ -3,6 +3,8 @@ This mod should fix some annoying issues with Ultimate Chicken Horse.
 
 Implemented fixes:
 1. Keep Audio running when game is in background (e.g. other window is in focus)
+1. Added Configuration Option to change the respawn time in Freeplay mode
+    - Added dependency to ConfigurationManager (press F1 in game to change available configurations)
 
 ## Credits
 - [Clever Endeavour Games](https://www.cleverendeavourgames.com/)
